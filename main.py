13c1998e168b37d444dae3936d1829cd842ad077
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-15 16:42:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from PIL import Image
+from matplotlib import cm
+import numpy as np
 
 image = Image.open('dataset-cover.jpg')
 df = pd.read_csv('Breast_Cancer.csv')
@@ -32,6 +34,42 @@ def get_mortality_rate(feature_name):
 def build_st_query_for_line_charts(title: str, options: list):
     feature = st.radio(f'Select {title}', options)
     return feature
+
+def build_st_query_for_ridge_charts(title: str, options: list):
+    st.write(f"#### {title}")
+    checkbox_states = {}
+    # Add "Select All" checkbox
+    if title == 'Age':
+        select_all = st.checkbox("All Ages")
+    elif title == 'Race':
+        select_all = st.checkbox("All Races")
+    else:
+        select_all = st.checkbox("All Marital Statuses")
+    checkbox_states = {}  # Dictionary to store checkbox states
+
+    if select_all:
+        for option in options:
+            checkbox_key = f"{option} ({title})"
+            checkbox_states[option] = st.checkbox(option, key=checkbox_key, value=True)
+    else:
+        for option in options:
+            checkbox_key = f"{option} ({title})"
+            checkbox_states[option] = st.checkbox(option, key=checkbox_key, value=checkbox_states.get(option, False))
+
+    return checkbox_states
+
+def create_virdis(num):
+    viridis = cm.get_cmap('viridis', 12)
+    virdis_list = viridis(np.linspace(0, 1, num))
+
+    modified_array = []
+    for lst in virdis_list:
+        modified_list = lst[:-1]  # Remove the last element from the list
+        rgb_string = f'rgb({modified_list[0]}, {modified_list[1]}, {modified_list[2]})'
+        modified_array.append(rgb_string)
+
+    return modified_array
+
 
 def build_heatmap():
     st.subheader('Impact of demographic characteristics on the mortality of women with breast cancer in America')
@@ -113,54 +151,105 @@ def build_two_y_axis_chart():
     )
     st.plotly_chart(fig)
 
+# def figure3():
+#     st.subheader('Women with which characteristics are more likely to have a short recovery from breast cancer?')
+#
+#     survived = df[df['Status'] == 'Alive']
+#     survived_avg = survived.groupby(['Marital Status', 'Race', 'Age'])['Survival Months'].mean().reset_index()
+#     color_scale = px.colors.qualitative.T10
+#
+#     fig = px.bar(survived_avg, x="Marital Status", y="Survival Months", color="Race",
+#                  animation_frame="Age", animation_group="Marital Status", facet_col="Race", range_y=[0, 100], color_discrete_sequence=color_scale)
+#     fig.update_layout(yaxis=dict(title=dict(text="Recovery Time (Months)")),height=600, width=900)
+#     fig.update_layout(
+#         updatemenus=[
+#             dict(
+#                 type="buttons",
+#                 buttons=[
+#                     dict(
+#                         label="Play",
+#                         method="animate",
+#                         args=[
+#                             None,
+#                             {
+#                                 "frame": {"duration": 1000},  # Frame duration for "Play" button
+#                                 "fromcurrent": True,
+#                                 "transition": {"duration": 500, "easing": "linear"},
+#                             },
+#                         ],
+#                     ),
+#                     dict(
+#                         label="Stop",
+#                         method="animate",
+#                         args=[
+#                             [None],
+#                             {
+#                                 "frame": {"duration": 0},  # Frame duration for "Stop" button
+#                                 "mode": "immediate",
+#                                 "transition": {"duration": 0},
+#                             },
+#                         ],
+#                     ),
+#                 ],
+#             ),
+#         ],
+#     )
+#     st.plotly_chart(fig)
+
 def figure3():
     st.subheader('Women with which characteristics are more likely to have a short recovery from breast cancer?')
+    st.markdown('### Select Characteristics')
+    col1, col2, col3 = st.columns(3)
+
+
+    with col1:
+        age_dict = build_st_query_for_ridge_charts(
+            "Age", ['30-39', '40-49', '50-59', '60-69']
+        )
+
+    with col2:
+        race_dict = build_st_query_for_ridge_charts(
+            "Race", ['White', 'Black', 'Other']
+        )
+
+    with col1:
+        marital_dict = build_st_query_for_ridge_charts(
+            "Marital Status", ['Married', 'Divorced', 'Single ', 'Widowed', 'Separated']
+        )
+
+    fig = go.Figure()
+
+    grouped = df.groupby(['Age', 'Race', 'Marital Status']).size().reset_index(name='count')
+    filtered_groups = grouped[grouped['count'] >= 2]
+    num_of_colors = len(filtered_groups)
+    colors = create_virdis(num_of_colors)
+    i = 0
 
     survived = df[df['Status'] == 'Alive']
-    survived_avg = survived.groupby(['Marital Status', 'Race', 'Age'])['Survival Months'].mean().reset_index()
-    color_scale = px.colors.qualitative.T10
+    grouped = survived.groupby(['Age', 'Race', 'Marital Status'])['Survival Months'].mean().reset_index()
 
-    fig = px.bar(survived_avg, x="Marital Status", y="Survival Months", color="Race",
-                 animation_frame="Age", animation_group="Marital Status", facet_col="Race", range_y=[0, 100], color_discrete_sequence=color_scale)
-    fig.update_layout(yaxis=dict(title=dict(text="Recovery Time (Months)")),height=600, width=900)
-    fig.update_layout(
-        updatemenus=[
-            dict(
-                type="buttons",
-                buttons=[
-                    dict(
-                        label="Play",
-                        method="animate",
-                        args=[
-                            None,
-                            {
-                                "frame": {"duration": 1000},  # Frame duration for "Play" button
-                                "fromcurrent": True,
-                                "transition": {"duration": 500, "easing": "linear"},
-                            },
-                        ],
-                    ),
-                    dict(
-                        label="Stop",
-                        method="animate",
-                        args=[
-                            [None],
-                            {
-                                "frame": {"duration": 0},  # Frame duration for "Stop" button
-                                "mode": "immediate",
-                                "transition": {"duration": 0},
-                            },
-                        ],
-                    ),
-                ],
-            ),
-        ],
-    )
+    # Sort the groups by the mean survival months in descending order
+    sorted_groups = grouped.sort_values('Survival Months', ascending=True)
+
+    # Iterate through each group
+    for _, row in sorted_groups.iterrows():
+        age = row['Age']
+        race = row['Race']
+        marital_status = row['Marital Status']
+        values = survived[
+            (survived['Age'] == age) & (survived['Race'] == race) & (survived['Marital Status'] == marital_status)][
+            'Survival Months']
+        if len(values) > 1:
+            fig.add_trace(go.Violin(x=values, line_color=colors[i], name=f'{age}, {race}, {marital_status}',
+                                    meanline_visible=True))
+            i += 1
+
+    fig.update_layout(legend=dict(traceorder='reversed', itemsizing='constant'))
+    fig.update_traces(orientation='h', side='positive', width=5, points=False)
+    fig.update_layout(xaxis_showgrid=False, xaxis_zeroline=False, xaxis_title='Survival Months')
+    fig.update_layout(violinmode='group', width=800, height=1000, xaxis_range=[0, 145])
+    fig.update_layout(yaxis=dict(showticklabels=False))  # Remove y-axis tick labels
     st.plotly_chart(fig)
-
-
-
-
 
 st.markdown("""
     <h1 style='text-align: center;'>Visualization Final Project</h1>
