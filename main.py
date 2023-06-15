f61058dc4810d003fca58663173dd8c230cd8220
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-15 19:21:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ def figure3():
                                     meanline_visible=True))
             i += 1
 
-    fig.line(df, hover_data=["Age"])
+    fig.line(survived, hover_data=['Race','Age'])
     fig.update_traces(orientation='h', side='positive', width=5, points=False, hoverinfo='skip')
     fig.update_layout(legend=dict(traceorder='reversed', itemsizing='constant'))
     fig.update_layout(xaxis_showgrid=False, xaxis_zeroline=False, xaxis_title='Survival Months')
