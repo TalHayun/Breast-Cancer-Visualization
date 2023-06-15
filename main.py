b87b055004b2c07270423c6498c1e0477387c98e
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-15 19:24:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -251,6 +251,7 @@ def figure3():
     fig.update_layout(xaxis_showgrid=False, xaxis_zeroline=False, xaxis_title='Survival Months')
     fig.update_layout(violinmode='group', width=800, height=1000, xaxis_range=[0, 145])
     fig.update_layout(yaxis=dict(showticklabels=False))  # Remove y-axis tick labels
+    fig.update_layout(hovermode="x")
     st.plotly_chart(fig)
 
 st.markdown("""
