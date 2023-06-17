8637245d79f9fcc695df2139fbe02b60380fcd3b
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 15:38:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -315,7 +315,12 @@ def figure3():
     #   st.plotly_chart(marital_graph, height=200, width=300)
     # with col1:
     #   st.plotly_chart(ridge, height=400, width=200) 
-    fig = sp.make_subplots(rows=3, cols=1)
+    fig = sp.make_subplots(rows=1, cols=1)
+    fig.add_trace(age_graph.data[0], row=1, col=1)
+    fig.update_layout(height=800, width=600)
+    st.plotly_chart(fig)
+
+
 
 st.markdown("""
     <h1 style='text-align: center;'>Visualization Final Project</h1>
