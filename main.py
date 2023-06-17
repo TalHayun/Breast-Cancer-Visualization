72d6ecf94800b63f1827bedf37a0c21b03e2ff0e
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 14:09:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import altair as alt
 import seaborn as sns
 import plotly.express as px
+import plotly.subplots as make_subplots
 import plotly.graph_objects as go
 from PIL import Image
 from matplotlib import cm
