ad6df7a43b48de47353bab6233796f7e012bba1d
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 13:50:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -309,11 +309,11 @@ def figure3():
     col1, col2 = st.columns([1, 2])
   
     with col2:
-      st.plotly_chart(age_graph, height=1, width=1)
-      st.plotly_chart(race_graph, height=1, width=1)
-      st.plotly_chart(marital_graph, height=1, width=1)
+      st.plotly_chart(age_graph, height=200, width=300)
+      st.plotly_chart(race_graph, height=200, width=300)
+      st.plotly_chart(marital_graph, height=200, width=300)
     with col1:
-      st.plotly_chart(ridge, height=1, width=1) 
+      st.plotly_chart(ridge, height=400, width=200) 
 
       
 
