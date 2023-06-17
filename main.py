51d3b79691fae5a1856f7e60fe755d21ff944e04
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 13:55:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -319,7 +319,7 @@ def figure3():
     # fig.add_trace(marital_graph)
     # fig.update_layout(height=600, width=800, title_text='Combined Graphs')
     # st.plotly_chart(fig)
-    st.write(marital_graph)
+    st.write(type(marital_graph))
 
 st.markdown("""
     <h1 style='text-align: center;'>Visualization Final Project</h1>
