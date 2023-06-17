319a09895dae20fbe010f05f5190e15da0f52881
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 15:53:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ def figure3():
     # with col1:
     #   st.plotly_chart(ridge, height=400, width=200) 
   
-    col1, col2 = st.beta_columns([3, 1])
+    col1, col2 = col1, col2 = st.columns([3, 1])
     with col1:
       st.plotly_chart(age_graph)
       st.plotly_chart(race_graph)
