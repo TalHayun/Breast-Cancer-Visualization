d63b8715ee74b61f152b7896a0fa9d776205c0ce
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 14:02:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -316,9 +316,9 @@ def figure3():
     #   st.plotly_chart(ridge, height=400, width=200) 
 
     fig = make_subplots(rows=1, cols=3)
-    fig.add_traces(age_graph.data[0], row=1, col=1)
-    fig.add_traces(race_graph.data[0], row=1, col=2)
-    fig.add_traces(marital_graph.data[0], row=1, col=3)
+    fig.add_traces(age_graph.data, row=1, col=1)
+    fig.add_traces(race_graph.data, row=1, col=2)
+    fig.add_traces(marital_graph.data, row=1, col=3)
     fig.update_layout(height=600, width=800, title_text='Combined Graphs')
     st.plotly_chart(fig)
 
