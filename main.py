bf062db76127c200ef437bdae9dfd236a5bf441c
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-17 21:50:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -333,8 +333,28 @@ def figure3():
                       yaxis4_title='Recovery Probability',
                       legend_tracegroupgap=50
                       )
+    fig.update_layout(
+    height=900, width=900,
+    xaxis1_title='Time (Months)',
+    xaxis2_title='Time to Recover (Months)',
+    xaxis3_title='Time (Months)',
+    xaxis4_title='Time (Months)',
+    yaxis1_title='Recovery Probability',
+    yaxis2_title='',
+    yaxis3_title='Recovery Probability',
+    yaxis4_title='Recovery Probability',
+    legend_tracegroupgap=50,
+    legend=dict(
+        x=0.5,  # X-axis position of the legend (0.0 to 1.0)
+        y=1.2,  # Y-axis position of the legend (0.0 to 1.0)
+        xanchor='center',  # Anchor point of the legend on the X-axis
+        yanchor='top',  # Anchor point of the legend on the Y-axis
+        orientation='h'  # Orientation of the legend (h for horizontal, v for vertical)
+    )
+)
 
-    st.plotly_chart(fig)
+st.plotly_chart(fig)
+
 
 
 st.markdown("""
