2a1f85e56058d922ecefd909183b419741c563c7
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-18 11:12:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,18 +94,18 @@ def build_heatmap():
           y=mortality_df['Mortality Rate'],
           marker=dict(color='salmon')
       ))
-    # bar_fig.update_layout(
-    #     yaxis=dict(title=dict(text= "Mortality Rate (%)", font=dict(size=20))),
-    #     xaxis=dict(title=dict(text=f'{feature1}', font=dict(size=20))))
-    # st.plotly_chart(bar_fig)
-
     bar_fig.update_layout(
-      yaxis=dict(title=dict(text="Mortality Rate (%)", font=dict(size=20))),
-      xaxis=dict(
-          title=dict(text=f'{feature1}', font=dict(size=20)),
-          tickfont=dict(size=16)  # Adjust the font size as needed
-      )
-  )
+        yaxis=dict(title=dict(text= "Mortality Rate (%)", font=dict(size=20))),
+        xaxis=dict(title=dict(text=f'{feature1}', font=dict(size=20))))
+    st.plotly_chart(bar_fig)
+
+  #   bar_fig.update_layout(
+  #     yaxis=dict(title=dict(text="Mortality Rate (%)", font=dict(size=20))),
+  #     xaxis=dict(
+  #         title=dict(text=f'{feature1}', font=dict(size=20)),
+  #         tickfont=dict(size=16)  # Adjust the font size as needed
+  #     )
+  # )
 
 
     col2 = st.columns(1)
