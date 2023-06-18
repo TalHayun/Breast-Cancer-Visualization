35edbb57568a956fe562636044ba5d15d60ab82d
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-18 11:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def build_heatmap():
     fig.update_layout(
     yaxis=dict(title=dict(text=f"<b>{feature1}</b>", font=dict(size=24))),
     xaxis=dict(title=dict(text=f"<b>{feature2}</b>", font=dict(size=24))),
-    coloraxis_colorbar=dict(title=dict(text='Mortality <br> rate (%)', font=dict(size=22)))
+    coloraxis_colorbar=dict(title=dict(text='Mortality <br> Rate (%)', font=dict(size=22)))
 )
     # Display the heatmap in Streamlit
     st.plotly_chart(fig)
