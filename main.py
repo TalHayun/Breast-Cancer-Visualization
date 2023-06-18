c1b5202d27849873d29cab8e7fd7947c09e30d0d
TalHayun <87673883+TalHayun@users.noreply.github.com>
2023-06-18 10:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -320,8 +320,8 @@ def figure3():
     fig.update_yaxes(showticklabels=False, row=1, col=2)
 
     # Violin positive
-    fig.update_traces(orientation='h', side='positive', width=5, points=False, row=1, col=2)
-    st.plotly_chart(fig)
+    # fig.update_traces(orientation='h', side='positive', width=5, points=False, row=1, col=2)
+    # st.plotly_chart(fig)
 
 
 
